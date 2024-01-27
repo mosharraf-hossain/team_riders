6ae2de33f2d575a4645268cab34672665c7bfd17
mosharraf-hossain <md.hossain3@stud.fra-uas.de>
2024-01-27 04:08:19 +0100

    testing multisequencelearning.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Dictionary<string, List<double>> mysequences = new Dictionary<string, List<double>>();
+        
+        //have to make class for reading data values
+
+        mysequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, }));
+        mysequences.Add("S2", new List<double>(new double[] { 8.0, 1.0, 2.0, 9.0, 10.0, 7.0, 11.00 }));
+        MultiSequenceLearning myexperiment = new MultiSequenceLearning();
+        var predictor = myexperiment.Run(mysequences);
+
+
     }
 }
