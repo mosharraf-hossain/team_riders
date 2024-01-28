4cdf85fc3b106f0bcee934bb62ca5c90c783d4d1
Zahid5156 <md.hasan5@stud.fra-uas.de>
2024-01-28 03:55:46 +0100

    Trying to Implement Json File Reader

--- Program.cs
+++ Program.cs
@@ -4,14 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Dictionary<string, List<double>> mysequences = new Dictionary<string, List<double>>();
+        /*Dictionary<string, List<double>> mysequences = new Dictionary<string, List<double>>();
         
         //have to make class for reading data values
 
         mysequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, }));
         mysequences.Add("S2", new List<double>(new double[] { 8.0, 1.0, 2.0, 9.0, 10.0, 7.0, 11.00 }));
         MultiSequenceLearning myexperiment = new MultiSequenceLearning();
-        var predictor = myexperiment.Run(mysequences);
+        var predictor = myexperiment.Run(mysequences);*/
+
+        JsonFileReader jsonReader = new JsonFileReader();
+
+        // Access the sequences data
+        var sequences = jsonReader.Sequences;
+
+        // Display the sequences
+        foreach (var sequence in sequences)
+        {
+            Console.WriteLine("Sequence:");
+            foreach (var number in sequence)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
 
 
     }
---
+++ jsonfileread.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace anomalydetectionapp
+{
+    public class JsonFileReader
+    {
+        private string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "file.json");
+        private List<List<int>> sequences;
+
+        public JsonFileReader()
+        {
+            ReadJsonFile();
+        }
+
+        private void ReadJsonFile()
+        {
+            // Read the JSON file
+            string json = File.ReadAllText(filePath);
+
+            // Deserialize the JSON into a list of lists of integers
+            sequences = JsonConvert.DeserializeObject<Dictionary<string, List<List<int>>>>(json)["sequences"];
+        }
+
+        // Property to expose the 'sequences' data
+        public List<List<int>> Sequences
+        {
+            get { return sequences; }
+        }
+    }
+}
