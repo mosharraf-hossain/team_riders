0d01b947aac330e5829d4aab25fa31e28acbca1f
Zahid5156 <md.hasan5@stud.fra-uas.de>
2024-03-24 02:11:44 +0100

    Added some comments to the codes for more understanding

--- Program.cs
+++ Program.cs
@@ -1,27 +1,40 @@
-﻿namespace anomalydetectionapp
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace anomalydetectionapp
 {
+    /// <summary>
+    /// Class containing methods to read sequences from JSON files, train a model, and detect anomalies.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The entry point of the application.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
             Dictionary<string, List<double>> mysequences = new Dictionary<string, List<double>>();
 
+            // Get the solution directory path
             string solutionDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
 
+            // Construct paths for training and predicting folders
             string trainingfolderPath = Path.Combine(solutionDirectory, "training_files");
-
             string predictingfolderPath = Path.Combine(solutionDirectory, "predicting_files");
 
+            // Read sequences from training and predicting folders
             var trainingjsonReader = new JsonFolderReader(trainingfolderPath);
-
             var predictingjsonreader = new JsonFolderReader(predictingfolderPath);
 
             var sequencesContainers = trainingjsonReader.AllSequences;
-
             var sequencesContainers1 = predictingjsonreader.AllSequences;
 
             int sequenceIndex = 1;
 
+            // Process sequences from the training folder
             foreach (var sequencesContainer in sequencesContainers)
             {
                 var sequences = sequencesContainer.Sequences;
@@ -33,14 +46,15 @@
                     string sequenceKey = "S" + sequenceIndex;
                     mysequences.Add(sequenceKey, convertedSequence);
                     sequenceIndex++;
-
                 }
             }
 
+            // Train the model using MultiSequenceLearning
             MultiSequenceLearning myexperiment = new MultiSequenceLearning();
             var predictor = myexperiment.Run(mysequences);
             predictor.Reset();
 
+            // Detect anomalies in sequences from the predicting folder
             foreach (var sequencesContainer in sequencesContainers1)
             {
                 var sequences = sequencesContainer.Sequences;
